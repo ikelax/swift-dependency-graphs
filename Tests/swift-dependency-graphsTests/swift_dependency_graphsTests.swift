72415f98007fd97100ba6ad6ed247a8645494f3f
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-10-02 18:22:22 +0200

    initiliaze repository with Swift Package Manager

---
+++ Tests/swift-dependency-graphsTests/swift_dependency_graphsTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import swift_dependency_graphs
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
