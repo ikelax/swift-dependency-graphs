bd38845c2811182c07abafa2650063b1726f9d59
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-10-06 13:19:12 +0200

    Add DependencyGraph.contains
    Methods that check for membership of vertices are required for more
    sophisticated methods. Examples for such methods are adding a vertex or
    finding all neighbours of a vertex.  When adding a vertex, we first want
    to make sure that the vertex does not exist already because we do not
    want to override its entries in the dictionaries.
    
    TestGraph was added to avoid code duplication. By using graphs with
    well-studied properties, we reduce the errors in the test
    specifications. For example, cyclic graphs are not acyclic. For custom
    graphs, we have to check this ourselves.
    
    Quick and Nimble were added because they improve the readability of
    tests.
    
    Related to: #4
    See also: #1

---
+++ Sources/SwiftDependencyGraphs/DependencyGraph.swift
@@ -0,0 +1,40 @@
+public struct DependencyGraph<V> where V: Hashable, V: Identifiable {
+    typealias Edge = (V, V)
+    
+    /// The vertices of the dependency graph.
+    public internal(set) var vertices: [V.ID: V] = [:]
+    
+    // For efficiency, two hashsets are maintained.
+    /**
+     Incoming edges of a vertex are directed edges that the vertex is the destination.
+     
+     The edge `v --> w` (or `(v, w)`) is an incoming edge of `w`, but not of `v`.
+     In this case, `w` is a key and `v` its value.
+     
+     - Note: Informally speaking, `v` is adjacent to `w`, or a neighbour of `w`, and
+     `w` is targeted by `v`'s arrow.
+     */
+    public internal(set) var incoming_edges: [V.ID: Set<V>] = [:]
+    
+    /**
+     Outgoing edges of a vertex are directed edges that the vertex is the origin.
+     
+     The edge `v --> w` (or `(v, w)`) is an outgoing edge of `v`, but not of `w`.
+     In this case, `v` is a key and `w` its value.
+     
+     - Note: Informally speaking, `v` is adjacent to `w`, or a neighbour of `w`, and
+     its arrow points to `w`.
+     */
+    public internal(set) var outgoing_edges: [V.ID: Set<V>] = [:]
+    
+    func contains(vertex: V) -> Bool {
+        return contains(vertexWith: {v in v.id == vertex.id})
+    }
+    
+    /**
+     Returns `true` iff at least one vertex satifies the `predicate`.
+     */
+    func contains(vertexWith predicate: (V) -> Bool) -> Bool {
+        return vertices.contains(where: {_, vertex in predicate(vertex)})
+    }
+}
---
+++ Tests/SwiftDependencyGraphsTests/Helpers/Vertex.swift
@@ -0,0 +1,22 @@
+/**
+ In comments or descriptions, a vertex with `id` `n` is referred to as `v${n}`.
+ Just using the `id` or the `label` may be confusing.
+ */
+struct Vertex: Identifiable, Hashable {
+    let id: Int
+    /// `label` is used for testing how the library handles properties.
+    let label: String
+}
+
+let vertex_1 = Vertex(id: 1)
+let vertex_2 = Vertex(id: 2)
+let vertex_3 = Vertex(id: 3)
+let vertex_4 = Vertex(id: 4)
+let vertex_5 = Vertex(id: 5)
+
+extension Vertex {
+    /// Convenience to also use `id` to set `label`.
+    init(id: Int) {
+        self.init(id: id, label: "\(id)")
+    }
+}
