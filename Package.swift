f205a6e562b337a75ecf28ea74b03233908bc3b4
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-10-12 10:55:44 +0200

    Replace Set with OrderedSet
    Using ordered sets in the dictionaries for vertices will be useful in
    tests for depth-first search. Ordered sets allow us to specify in which
    order the graph will traverse the vertices.
    
    Related to: https://github.com/ikelax/swift-dependency-graphs/issues/1

--- Package.swift
+++ Package.swift
@@ -15,12 +15,20 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
+        .package(
+              url: "https://github.com/apple/swift-collections.git",
+              .upToNextMinor(from: "1.1.0")
+            )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-dependency-graphs"),
+            name: "swift-dependency-graphs",
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections")
+            ]
+        ),
         .testTarget(
             name: "swift-dependency-graphsTests",
             dependencies: ["swift-dependency-graphs", "Quick", "Nimble"]
