de75c5cc7367e919a88723311641543e1358529b
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-11-17 18:54:41 +0100

    Use kebab-case for package name
    This is more consistent with other Swift packages like
    https://github.com/apple/swift-collections.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "DependencyGraphs",
+    name: "swift-dependency-graphs",
     platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
