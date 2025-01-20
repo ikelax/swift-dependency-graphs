25ca8be68a4990807c5f1841c2dc485a4a354cad
ikelax <163678144+ikelax@users.noreply.github.com>
2025-01-20 19:03:06 +0100

    Add CI pipeline (#14)
    Add CI pipeline
    
    The pipeline runs the test on macOS and Ubuntu. It checks the formatting
    and linting of the code with SwiftLint and swift-format. The spelling is
    checked by Typos.
    
    After, I have migrated all tests to swift-testing from Quick and Nimble,
    I will add a job that runs the tests on Windows. Nimble uses
    CoreFoundation which is not supported on Windows. swift-testing is
    supported on Windows.
    
    xcbeautify is used as reporter for failed tests. I was not able to find
    out how to set the path for Linuxbrew/Homebrew such that the next runner
    finds xcbeautify. So, I added two separate jobs and run the commands for
    the Ubuntu job in one runner.
    
    swift-format and SwiftLint have different default for trailing commata.
    Because I prefer trailing commata, I disabled the rule from SwiftLint.
    
    I fixed the spelling, formatting and linting errors found by the tools.
    
    See also
    
    For SwiftLint command
    https://github.com/realm/SwiftLint/blob/main/.github/workflows/lint.yml
    
    For xcbeautify command in GitHub Action
    https://github.com/cpisciotta/xcbeautify
    
    For Typos Action
    https://github.com/crate-ci/typos/blob/master/docs/github-action.md
    
    Swift-testing support on Windows
    https://github.com/swiftlang/swift-testing
    
    Migration to swift-testing
    https://github.com/ikelax/swift-dependency-graphs/issues/15
    
    Related to https://github.com/ikelax/swift-dependency-graphs/issues/2
    See pull request https://github.com/ikelax/swift-dependency-graphs/pull/14

--- Package.swift
+++ Package.swift
@@ -4,34 +4,35 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-dependency-graphs",
-    platforms: [.macOS(.v10_15)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "DependencyGraphs",
-            targets: ["DependencyGraphs"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
-        .package(
-              url: "https://github.com/apple/swift-collections.git",
-              .upToNextMinor(from: "1.1.0")
-            )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "DependencyGraphs",
-            dependencies: [
-                .product(name: "Collections", package: "swift-collections")
-            ]
-        ),
-        .testTarget(
-            name: "DependencyGraphsTests",
-            dependencies: ["DependencyGraphs", "Quick", "Nimble"]
-        ),
-    ]
+  name: "swift-dependency-graphs",
+  platforms: [.macOS(.v10_15)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "DependencyGraphs",
+      targets: ["DependencyGraphs"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
+    .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
+    .package(
+      url: "https://github.com/apple/swift-collections.git",
+      .upToNextMinor(from: "1.1.0")
+    ),
+    .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.58.2"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "DependencyGraphs",
+      dependencies: [
+        .product(name: "Collections", package: "swift-collections")
+      ]
+    ),
+    .testTarget(
+      name: "DependencyGraphsTests",
+      dependencies: ["DependencyGraphs", "Quick", "Nimble"]
+    ),
+  ]
 )
--- Tests/SwiftDependencyGraphsTests/Helpers/Vertex.swift
+++ Tests/SwiftDependencyGraphsTests/Helpers/Vertex.swift
@@ -1,26 +1,24 @@
-/**
- In comments or descriptions, a vertex with `id` `n` is referred to as `v${n}`.
- Just using the `id` or the `label` may be confusing.
- */
+/// In comments or descriptions, a vertex with `id` `n` is referred to as `v${n}`.
+/// Just using the `id` or the `label` may be confusing.
 struct Vertex: Identifiable, Hashable {
-    let id: Int
-    /// `label` is used for testing how the library handles properties.
-    let label: String
+  let id: Int
+  /// `label` is used for testing how the library handles properties.
+  let label: String
 }
 
-let vertex_1 = Vertex(id: 1)
-let vertex_2 = Vertex(id: 2)
-let vertex_3 = Vertex(id: 3)
-let vertex_4 = Vertex(id: 4)
-let vertex_5 = Vertex(id: 5)
-let vertex_6 = Vertex(id: 6)
-let vertex_7 = Vertex(id: 7)
+let vertex1 = Vertex(id: 1)
+let vertex2 = Vertex(id: 2)
+let vertex3 = Vertex(id: 3)
+let vertex4 = Vertex(id: 4)
+let vertex5 = Vertex(id: 5)
+let vertex6 = Vertex(id: 6)
+let vertex7 = Vertex(id: 7)
 
 let emptyVertexList: [Vertex] = []
 
 extension Vertex {
-    /// Convenience to also use `id` to set `label`.
-    init(id: Int) {
-        self.init(id: id, label: "\(id)")
-    }
+  /// Convenience to also use `id` to set `label`.
+  init(id: Int) {
+    self.init(id: id, label: "\(id)")
+  }
 }
