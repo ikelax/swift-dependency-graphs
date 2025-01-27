3d8ebe9ec2fa6c1ab5f9296edf67c082fc492f7d
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-01-27 16:36:40 +0100

    Migrate Tests/Dfs/Path.swift to swift-testing
    Related to https://github.com/ikelax/swift-dependency-graphs/issues/15

--- Tests/SwiftDependencyGraphsTests/Dfs/Path.swift
+++ Tests/SwiftDependencyGraphsTests/Dfs/Path.swift
@@ -1,58 +1,48 @@
-import Nimble
-import Quick
+import Testing
 
 @testable import DependencyGraphs
 
-class DfsPathTests: QuickSpec {
-  override class func spec() {
-    describe("starting from vertex 1 in forwards direction") {
-      describe("visits all vertices") {
-        it("with the public method") {
-          expect(
-            TestGraph.path().depthFirstSearch(
-              startingFrom: vertex1, in: .forwards, reduceWith: appendReducer, withInitialValue: []
-            )
-          )
-          .to(equal([vertex1, vertex2, vertex3, vertex4, vertex5]))
-        }
-
-        it("with the private method") {
-          expect(
-            TestGraph.path().depthFirstSearchImpl(
-              startingFrom: vertex1, in: .forwards, withVisited: [], reduceWith: appendReducer,
-              withInitialValue: [])
-          )
-          .to(equal([vertex1, vertex2, vertex3, vertex4, vertex5]))
-        }
-      }
-
-      it("stops at 2 because 3 was already discovered") {
-        expect(
-          TestGraph.path().depthFirstSearchImpl(
-            startingFrom: vertex1, in: .forwards, withVisited: [vertex3],
-            reduceWith: appendReducer, withInitialValue: [])
+@Suite("starting from vertex 1 in forwards direction") struct DfsPathTests {
+  @Suite("visits all vertices") struct VisitAllVerticesTests {
+    @Test("with the public method") func dfs() {
+      #expect(
+        TestGraph.path().depthFirstSearch(
+          startingFrom: vertex1, in: .forwards, reduceWith: appendReducer, withInitialValue: []
         )
-        .to(equal([vertex1, vertex2]))
-      }
-
-      it("visits no vertices because all were already discovered") {
-        expect(
-          TestGraph.path().depthFirstSearchImpl(
-            startingFrom: vertex1, in: .forwards,
-            withVisited: [vertex1, vertex2, vertex3, vertex4, vertex5],
-            reduceWith: appendReducer, withInitialValue: emptyVertexList)
-        )
-        .to(equal([]))
-      }
-
-      it("visits no vertices because 1 was already discovered") {
-        expect(
-          TestGraph.path().depthFirstSearchImpl(
-            startingFrom: vertex1, in: .forwards, withVisited: [vertex1],
-            reduceWith: appendReducer, withInitialValue: emptyVertexList)
-        )
-        .to(equal([]))
-      }
+          == [vertex1, vertex2, vertex3, vertex4, vertex5])
     }
+
+    @Test("with the private method") func dfsImpl() {
+      #expect(
+        TestGraph.path().depthFirstSearchImpl(
+          startingFrom: vertex1, in: .forwards, withVisited: [], reduceWith: appendReducer,
+          withInitialValue: [])
+          == [vertex1, vertex2, vertex3, vertex4, vertex5])
+    }
+  }
+
+  @Test("stops at 2 because 3 was already discovered") func stops() {
+    #expect(
+      TestGraph.path().depthFirstSearchImpl(
+        startingFrom: vertex1, in: .forwards, withVisited: [vertex3],
+        reduceWith: appendReducer, withInitialValue: [])
+        == [vertex1, vertex2])
+  }
+
+  @Test("visits no vertices because all were already discovered") func allDiscovered() {
+    #expect(
+      TestGraph.path().depthFirstSearchImpl(
+        startingFrom: vertex1, in: .forwards,
+        withVisited: [vertex1, vertex2, vertex3, vertex4, vertex5],
+        reduceWith: appendReducer, withInitialValue: emptyVertexList)
+        == [])
+  }
+
+  @Test("visits no vertices because 1 was already discovered") func nextDiscovered() {
+    #expect(
+      TestGraph.path().depthFirstSearchImpl(
+        startingFrom: vertex1, in: .forwards, withVisited: [vertex1],
+        reduceWith: appendReducer, withInitialValue: emptyVertexList)
+        == [])
   }
 }
