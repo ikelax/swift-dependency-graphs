22c5ddefad763556e73d3f8a522628beb37a8fe8
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-13 11:11:22 +0100

    Add documentation for addReducer

--- Tests/SwiftDependencyGraphsTests/Helpers/Reducers.swift
+++ Tests/SwiftDependencyGraphsTests/Helpers/Reducers.swift
@@ -5,6 +5,11 @@ func appendReducer<T>(visited: [T], vertex: T) -> [T] {
   return visited
 }
 
+/// Adds the `id` of `vertex` to `sum`.
+/// - Parameters:
+///   - sum: The original sum value.
+///   - vertex: The vertex with the `id` to add to `sum`.
+/// - Returns: The new sum which is `sum + vertex.id`.
 func addReducer(sum: Int, vertex: Vertex) -> Int {
   return sum + vertex.id
 }
