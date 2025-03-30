e0d6e31722b2b60b90b539b7297b8ad652490aa2
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-30 20:57:33 +0200

    Improve documentation for RemoveVertexSuccess
    The type RemovedEdges implies that the edges were removed.

--- Sources/SwiftDependencyGraphs/RemoveVertexSuccess.swift
+++ Sources/SwiftDependencyGraphs/RemoveVertexSuccess.swift
@@ -1,7 +1,14 @@
 import OrderedCollections
 
 public struct RemoveVertexSuccess<V: Hashable>: Equatable {
+  typealias RemovedEdges = OrderedSet<V>
+
+  /// The removed vertex.
   let vertex: V
-  let outgoingEdges: OrderedSet<V>
-  let incomingEdges: OrderedSet<V>
+  /// The outgoing edges that have been removed with the vertex. The set is only non-empty
+  /// if the vertex was removed by force.
+  let outgoingEdges: RemovedEdges
+  /// The incoming edges that have been removed with the vertex. The set is only non-empty
+  /// if the vertex was removed by force.
+  let incomingEdges: RemovedEdges
 }
